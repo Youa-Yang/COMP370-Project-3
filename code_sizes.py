bb4641cadd6010413286b77c0d73859dd6061d22
Dustin Nguyen <dustin7212@gmail.com>
2022-04-02 18:35:17 -0500

    Fix import

--- code_sizes.py
+++ code_sizes.py
@@ -1,10 +1,12 @@
-import config
-
 from github import Github
 import csv
 from os import environ
 
-g = Github(login_or_token=environ.get("ACCESS_TOKEN", config.access_token), per_page=100)
+if environ.get("ACCESS_TOKEN") is None:
+    import config
+
+g = Github(login_or_token=environ.get(
+    "ACCESS_TOKEN", config.access_token), per_page=100)
 
 
 def getCodeSize(sha):
@@ -27,6 +29,7 @@ with open('code_sizes.csv', 'a+', newline='') as file:
     progress = 0
     for commit in g.get_repo("ninja-build/ninja").get_commits()[current_commits:current_commits+100]:
         progress += 1
-        print(str(progress), "/ 100", [commit.commit.committer.date, commit.commit.tree.sha, getCodeSize(commit.commit.tree.sha)])
+        print(str(progress), "/ 100", [commit.commit.committer.date,
+              commit.commit.tree.sha, getCodeSize(commit.commit.tree.sha)])
         writer.writerow(
             [commit.commit.committer.date, commit.commit.tree.sha, getCodeSize(commit.commit.tree.sha)])
